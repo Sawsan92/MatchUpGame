ec1e1a0ad13a068cb91e3c3ccfbc3dc9123a824f
sawsan <sawsanemad92@gmail.com>
2015-04-01 19:47:03 +0200

    story board

---
+++ MatchUp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MatchUp
+//
+//  Created by JETS on 3/31/15.
+//  Copyright (c) 2015 JETS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ MatchUpTests/MatchUpTests.m
@@ -0,0 +1,34 @@
+//
+//  MatchUpTests.m
+//  MatchUpTests
+//
+//  Created by JETS on 3/31/15.
+//  Copyright (c) 2015 JETS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MatchUpTests : XCTestCase
+
+@end
+
+@implementation MatchUpTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
